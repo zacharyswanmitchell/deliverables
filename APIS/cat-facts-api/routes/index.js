93e9596ccc6d947bc84323c2b2c524365fb776e8
Zachary Mitchell <masspike84@yahoo.com>
2023-10-20 15:20:15 -0400

    API function working

---
+++ APIS/cat-facts-api/routes/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+const fetch = require("node-fetch");
+
+const ROOT_URL = "https://catfact.ninja";
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "CAT FACT" });
+});
+// GET random cat fact
+router.get("/cats/fact", function (req, res, next) {
+  fetch(`${ROOT_URL}/fact`)
+    .then(res => res.json())
+    .then(fact => res.render("cats/facts", { fact },));
+});
+
+module.exports = router;
